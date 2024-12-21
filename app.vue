29f12f03523308459e8bee0291c48981538b893f
GuillermoK1 <guillermojkr@gmail.com>
2024-12-21 15:16:42 +0000

    style: app.vue header

--- app.vue
+++ app.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="pt-2 w-dvw min-h-screen bg-slate-300 dark:bg-sky-900">
-    <div class="m-2 flex justify-between">
-      <h1 class="px-6 flex no-wrap">
-        About project powered by   
+    <div>
+      <div class="m-2 flex">
+        <ColorScheme><USelect class="w-24 mx-2 place-self-end overflow-visible" v-model="$colorMode.preference" :options="['system', 'light', 'dark']"/></ColorScheme>
+        <h1 class="flex no-wrap"></h1>
+          About project powered by
+      </div>
+      <div class="mx-2 flex">
         <img class="w-6 h-6 mx-2" src="/faviconNuxt.ico" alt="Nuxt logo"> 
-         NuxtUI and   
+          NuxtUI and
         <img class="w-6 h-6 mx-2" src="/faviconTailwind.png" alt="Tailwind logo"> 
-         Tailwind.css
-      </h1>
-      <ColorScheme><USelect class="w-24 place-self-end" v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
+          Tailwind.css
+      </div>
     </div>
     <NuxtLayout class="w-full min-h-screen bg-inherit"/>
   </div>
