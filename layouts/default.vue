7850be3013b83a0f81d37c9c027c7048ccabe3d7
GuillermoK1 <guillermojkr@gmail.com>
2025-04-11 17:45:14 +0100

    debugged the min-h-dvh.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="min-h-dvh  w-full topography p-4 grid grid-rows-[auto_1fr_auto] gap-12">
+    <section class="min-h-dvh w-full topography p-4 grid grid-rows-[auto_1fr_auto] gap-12">
         <LayoutHeader class="backdrop-blur-sm bg-slate-400/90 dark:bg-slate-600/90 ring-2 ring-black dark:ring-white h-fit w-full"/>
         <LayoutPages class="backdrop-blur-sm bg-slate-400/90 dark:bg-slate-600/90 ring-2 ring-black dark:ring-white rounded-lg min-h-72 w-full"/>
         <LayoutFooter class="backdrop-blur-sm bg-slate-400/90 dark:bg-slate-600/90 ring-2 ring-black dark:ring-white h-fit w-full"/>
